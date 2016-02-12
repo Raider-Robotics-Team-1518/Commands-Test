d01992f90929e0d45f86dd3537738163b6158111
Ben Kearns <kaitosakurai198@hotmail.com>
2016-02-11 21:36:04 -0500

    End of workday 11-FEB-16
    Autonomous mode 1 added and tested.  Test drove.  Need to fix logic of
    reverseDrive.

---
+++ CommandBasedTest/src/org/usfirst/frc1518/CommandBasedTest/commands/AutoMode1.java
@@ -0,0 +1,71 @@
+package org.usfirst.frc1518.CommandBasedTest.commands;
+
+
+import org.usfirst.frc1518.CommandBasedTest.Robot;
+import org.usfirst.frc1518.CommandBasedTest.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.*;
+
+public class AutoMode1 extends Command {
+
+	boolean taskComplete = false;
+
+	public AutoMode1() {
+	    requires(Robot.driveTrain);
+		// addSequential(new Forward( ));
+	 
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		int timeout = 0;
+		
+		while (timeout < 250000) {
+			Robot.driveTrain.drive(-.5, -.5);
+			timeout = timeout + 1;
+		}
+		Robot.driveTrain.drive(0, 0);
+		timeout = 0;
+		while (timeout <250000) {
+			Robot.driveTrain.drive(-.5, .5);
+			timeout = timeout + 1;
+		}
+		Robot.driveTrain.drive(0, 0);
+		timeout = 0;
+		while (timeout <250000) {
+			Robot.driveTrain.drive(-.5, -.5);
+			timeout = timeout + 1;
+		}
+		Robot.driveTrain.drive(0, 0);
+		
+		taskComplete = true;
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return taskComplete;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		Robot.driveTrain.drive(0, 0);
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+}
